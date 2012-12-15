c9556f67bbdcf76e990b1cecffb92f037fd11dbe
lvilnis <lvilnis@gmail.com>
2012-12-14 23:22:38 -0500

    Check in report and plot

--- report/plot_accuracies.m
+++ report/plot_accuracies.m
@@ -8,46 +8,49 @@ else
 	results = csvread(fullfile(pwd, 'results.csv'));
 end
 assert(size(results, 2) == 4);
-dataset_column = 1;
-classifier_column = 2;
-partition_column = 3;
-accuracy_column = 4;
 
+classifier_column = 1;
+partition_column = 2;
+accuracy_column = 3;
+dataset_column = 4;
+results = sortrows(results, partition_column)
 nDatasets = unique(results(:, dataset_column));
 nClassifiers = unique(results(:, classifier_column));
 nPartitions = unique(results(:, partition_column));
 color_str = {'bo-', 'ro-', 'go-', 'ko-', 'mo-', 'bs--', 'rs--', 'gs--', 'ks--', 'ms--'};
 assert(length(nClassifiers) <= numel(color_str));
-title_str = {'Dataset I', 'Dataset II', 'Dataset III'};
+title_str = {'Prediction For Closed Questions', 'Prediction For Open Questions', 'Dataset III'};
 
 figure(); set(gcf, 'Position', [10, 100, 1200, 800]);
-for d = 1:length(nDatasets)
+for d = 1:2
 	target_dataset = results(:, dataset_column) == nDatasets(d);
 	for c = 1:length(nClassifiers)
 		target_classifier = results(:, classifier_column) == nClassifiers(c);
-		subplot(3, 1, d); plot(1:length(nPartitions),...
+		subplot(2, 1, d); plot(results(target_dataset & target_classifier, partition_column),...
 				results(target_dataset & target_classifier, accuracy_column),...
 				color_str{c}, 'LineWidth', 2);
 		grid on;
-		if c == 1, hold on; ylim([50, 100]); end
-		x_tick_str = {results(target_dataset & target_classifier, partition_column)'};
-		set(gca, 'XTickLabel', x_tick_str);
-	end
-	if d == 1
-		title(sprintf('Predicting closed questions\n%s', title_str{d}),...
+		if c == 1, hold on; ylim([.6, .85]); end
+        %x_tick_str = { linspace(1,10)};
+		%x_tick_str = {results(target_dataset & target_classifier, partition_column)'};
+		%set(gca, 'XTickLabel', x_tick_str);
+    end
+    if d == 1
+        title(sprintf('F1 Scores With Increasing Data Set Size\n\n\n%s', title_str{d}),...
 					'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
-		h_legend = legend('svm(2)', 'Naive Bayes', 'Location', 'NorthWest', 'Orientation', 'Horizontal');
-		set(h_legend, 'FontSize', 9);
-	else
-		title(sprintf('%s', title_str{d}),...
+    end
+    if d == 2
+            title(sprintf( title_str{d}),...
 					'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
-	end
-	if d == 2
-		ylabel('Accuracies', 'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
-	end
-	if d == length(nDatasets)
-		xlabel('Training set partition', 'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
-	end
+    end
+    h_legend = legend('Naive Bayes', 'Log Loss w/ AdaGrad', 'Hinge Loss w/ AdaGrad','Liblinear SVM', 'Location', 'NorthWest', 'Orientation', 'Horizontal');
+	set(h_legend, 'FontSize', 9);
+	
+	ylabel('F1', 'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
+	
+    if d == 2
+        xlabel(sprintf('\n\n70/30 Train/Test Split'), 'FontSize', 13, 'FontWeight', 'b', 'FontName', 'Helvetica');
+    end
 end
 
 file_name = sprintf('images/stackoverflow_results');
